aa4e483bc214219a239e62ee7f5c2b96b9d7993d
ElynJin <elyn.jin@silksoftware.com>
2021-03-09 17:17:29 +0800

    #549: REMOVE Account Navigation from non-b2b page

--- assets/js/theme/bundleb2b.js
+++ assets/js/theme/bundleb2b.js
@@ -17,11 +17,36 @@ function fnCheckBeforeShoppinglistModal(context) {
         }
     });
 }
+function removeAccountNav() {
+    // add the following code
+    const inPages = () => {
+        const urlArray = [
+            '/account.php',
+            '/buy-again/',
+            '/address-book/',
+            '/create-quote/',
+            '/quote-detail/',
+            '/quote-edit/',
+            '/quote-list/',
+            '/dashboard/',
+            '/order-detail/',
+            '/quick-order-pad/',
+            '/shopping-list/',
+            '/shopping-lists/',
+            '/user-management/',
+        ];
+        const current = window.location.pathname;
+        return urlArray.includes(current);
+    };
+    if (!inPages()) {
+        $('body').addClass('non-b2b');
+    }
+}
 export default function (context) {
+    removeAccountNav();
     fnCheckBeforeShoppinglistModal(context);
 
     const url = 'https://cdn.bundleb2b.net/bundleb2b.2.10.0.js';
-    // const url = 'http://127.0.0.1:8080/bundleb2b.2.10.0.js';
     const el = document.createElement('script');
     el.setAttribute('src', url);
     document.querySelector('body').append(el);
